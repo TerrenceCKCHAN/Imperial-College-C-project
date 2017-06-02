2221ea877a3882b215c38836f26bc12aa340f88d
klc116 <klc116@ic.ac.uk>
2017-06-02 18:32:25 +0100

    added branch function with the updated instruction structure

--- src/branch.c
+++ src/branch.c
@@ -2,14 +2,12 @@
 // Created by admin on 30/5/2017.
 //
 
-
 #include "decode.h"
 #include "emulate.h"
 
-void branch(MACHINE* ARM, u32 instruction) {
-    BRANCH* branchInstr = DecodeBranch(instruction);
-    if(satisfyCondition(ARM, instruction)) {
-        u32 offset  = GETBITS(instruction, 23, 0);
+void branch(MACHINE* ARM, BRANCH_INSTR* br) {
+    if(satisfyCondition(ARM, br->INSTRUCTION)) {
+        u32 offset  = GETBITS(br->INSTRUCTION, 23, 0);
         u32 shifted = LShiftL(offset, 2);
         ARM->PCREG += shifted;
     }
