dd14c03f0e6d996814d89cf72cccc36adb1d0798
Cheuk Ki Fung <ki.cheuk16@imperial.ac.uk>
2017-06-06 16:57:44 +0100

    working on sdti, branch is still untouched

--- src/assembler/sin_data_transfer.c
+++ src/assembler/sin_data_transfer.c
@@ -3,12 +3,39 @@
 //
 #include "assemble.h"
 
-INSTRUCTION sin_data_transfer(LINE_TOKEN* token ){
+INSTRUCTION* sin_data_transfer(LINE_TOKEN* token ){
     INSTRUCTION* instr = malloc(sizeof(INSTRUCTION));
-    instr->instr->opcode = token->opcode;
+    SIN_DATA_TRAN_INSTR* sdtr = malloc (sizeof(SIN_DATA_TRAN_INSTR));
+    sdtr->OPCODE = token->operands[0];
+    sdtr->REGD = token->operands[1];
+    int isinrect=0;
+    for (int i = 2; i < token->numOfOperands+1; i++) {
+        isinexp=0;
+        isinrect=0;
+        if (token->*(operands+i)[0]=='='){
+            isinexp=1;
+        }
+        else if (token->*(operands+i)[0]=='['){
+            isinrect=1;
+        }
+
+        if(isinexp){
+            if(token->operands[0] == "ldr"){
+
+            }
+            else{
+                printf("Error: str with an numeric expression");
+            }
+        }
+        else if (isinrect){
+
+        }
+    }
+
+    instr->instr = *sdtr;
 
 
 
-    return INSTRUCTION;
+    return instr;
 
 }
