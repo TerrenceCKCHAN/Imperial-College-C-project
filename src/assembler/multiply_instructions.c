2c8da484c032f3103b298790d062de5df9bbdec2
ckc116 <ckc116@ic.ac.uk>
2017-06-06 20:23:43 +0100

    Multiply instructions are restructured

--- src/assembler/multiply_instructions.c
+++ src/assembler/multiply_instructions.c
@@ -2,23 +2,19 @@
 // Created by admin on 6/6/2017.
 //
 
-void multiplyInstructions (LINE_TOKEN *line_token) {
-    INSTRUCTION *instr = malloc(sizeof(INSTRUCTION));
-
-    // This is the case for the syntax mul Rd, Rm, Rs
-    if (line_token->operands[3] == NULL) {
-        instr->instr.mp->DEST = line_token->operands[0];
-        instr->instr.mp->REGM = line_token->operands[1];
-        instr->instr.mp->REGS = line_token->operands[2];
-        instr->instr.mp->A    = 0;
-        instr->instr.mp->S    = 0;
-
-    } else {
-        instr->instr.mp->DEST = line_token->operands[0];
-        instr->instr.mp->REGM = line_token->operands[1];
-        instr->instr.mp->REGS = line_token->operands[2];
-        instr->instr.mp->ACC  = line_token->operands[3];
-        instr->instr.mp->S    = 0;
-    }
+void assembleMul(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.mp->DEST = line_token->operands[0];
+    instr->instr.mp->REGM = line_token->operands[1];
+    instr->instr.mp->REGS = line_token->operands[2];
+    instr->instr.mp->A    = 0;
+    instr->instr.mp->S    = 0;
     instr->instr.mp->COND = 1110 << 28;
+}
+
+void assembleMla(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.mp->DEST = line_token->operands[0];
+    instr->instr.mp->REGM = line_token->operands[1];
+    instr->instr.mp->REGS = line_token->operands[2];
+    instr->instr.mp->ACC  = line_token->operands[3];
+    instr->instr.mp->S    = 0;
 }
