87ac3f7a8289ccc51a33328622188a738da374b6
ckc116 <ckc116@ic.ac.uk>
2017-06-06 20:55:16 +0100

    Restructure of data_processing functions and type compatibility for each function is corrected

--- src/assembler/data_processing.c
+++ src/assembler/data_processing.c
@@ -6,8 +6,72 @@
 #include "tokenizer.h"
 
 
-void expressionConverter(char *operand2, )
+void assembleAdd(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "add";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleSub(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "sub";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleRsb(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "rsb";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleAnd(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "and";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleEor(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "eor";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleOrr(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   = "orr";
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
+void assembleMov(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->DEST     = parseRegister(line_token->operands[0]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+    instr->instr.dp->COND     = 1110 << 28;
+}
 
+void assembleTst(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   ="tst";
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+}
+void assembleTeq(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   ="teq";
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+}
+void assembleCmp(LINE_TOKEN* line_token, INSTRUCTION* instr) {
+    instr->instr.dp->OPCODE   ="cmp";
+    instr->instr.dp->SRC      = parseRegister(line_token->operands[1]);
+    instr->instr.dp->OPERAND2 = parseExpression(line_token->operands[2]);
+}
+
+//This is the implementation for one single dataProcessing function that cover all the cases.
+/*
 INSTRUCTION *dataProcessing(LINE_TOKEN *line_token) {
     INSTRUCTION *instr = malloc(sizeof(INSTRUCTION));
 
@@ -34,4 +98,4 @@ INSTRUCTION *dataProcessing(LINE_TOKEN *line_token) {
     }
 
     return instr;
-}
+}*/
