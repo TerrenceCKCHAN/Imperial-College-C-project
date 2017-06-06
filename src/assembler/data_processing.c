b1d42aa840099f323724b387aaf76b1ba1d5e75f
ckc116 <ckc116@ic.ac.uk>
2017-06-06 18:30:18 +0100

    Multiply Instruction first draft (incompatible types, functions for type conversion to be defined in utils)

--- src/assembler/data_processing.c
+++ src/assembler/data_processing.c
@@ -5,21 +5,22 @@
 #include "assemble.h";
 #include "tokenizer.h"
 
-void dataProcessing(LINE_TOKEN *line_token) {
+
+void expressionConverter(char *operand2, )
+
+INSTRUCTION *dataProcessing(LINE_TOKEN *line_token) {
     INSTRUCTION *instr = malloc(sizeof(INSTRUCTION));
 
 
-    // This is the case for Instructions that do not compute results
-    if (line_token->operands[0] == NULL) {
+        // This is the case for Instructions that do not compute results
+    if (line_token->opcode == "tst" | "teq" | "cmp") {
         instr->instr.dp->SRC      = line_token->operands[1];
         instr->instr.dp->OPERAND2 = line_token->operands[2];
-
     }
         // This is the case for Single operand assignment:mov
-    else if (line_token->operands[1] == NULL) {
-
+    else if (line_token->opcode == "mov") {
         instr->instr.dp->DEST     = line_token->operands[0];
-        instr->instr.dp->OPERAND2 = line_token->operands[2]
+        instr->instr.dp->OPERAND2 = line_token->operands[2];
         instr->instr.dp->COND     = 1110 << 28;
 
     }
@@ -32,5 +33,5 @@ void dataProcessing(LINE_TOKEN *line_token) {
         instr->instr.dp->COND     = 1110 << 28;
     }
 
-
+    return instr;
 }
