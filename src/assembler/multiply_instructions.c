322c9318598f891d8b98e33a8f3ecfe5a8ce0ad6
ckc116 <ckc116@ic.ac.uk>
2017-06-06 20:36:05 +0100

    Parsers are used in Multiply (type is now compatible)

--- src/assembler/multiply_instructions.c
+++ src/assembler/multiply_instructions.c
@@ -2,19 +2,26 @@
 // Created by admin on 6/6/2017.
 //
 
+#include parser.h;
+#include assemble.h;
+#include tokenizer.h;
+
+
+
 void assembleMul(LINE_TOKEN* line_token, INSTRUCTION* instr) {
-    instr->instr.mp->DEST = line_token->operands[0];
-    instr->instr.mp->REGM = line_token->operands[1];
-    instr->instr.mp->REGS = line_token->operands[2];
+    instr->instr.mp->DEST = parseRegister(line_token->operands[0]);
+    instr->instr.mp->REGM = parseRegister(line_token->operands[1]);
+    instr->instr.mp->REGS = parseRegister(line_token->operands[2]);
     instr->instr.mp->A    = 0;
     instr->instr.mp->S    = 0;
     instr->instr.mp->COND = 1110 << 28;
 }
 
 void assembleMla(LINE_TOKEN* line_token, INSTRUCTION* instr) {
-    instr->instr.mp->DEST = line_token->operands[0];
-    instr->instr.mp->REGM = line_token->operands[1];
-    instr->instr.mp->REGS = line_token->operands[2];
-    instr->instr.mp->ACC  = line_token->operands[3];
+    instr->instr.mp->DEST = parseRegister(line_token->operands[0]);
+    instr->instr.mp->REGM = parseRegister(line_token->operands[1]);
+    instr->instr.mp->REGS = parseRegister(line_token->operands[2]);
+    instr->instr.mp->ACC  = parseRegister(line_token->operands[3]);
     instr->instr.mp->S    = 0;
+    instr->instr.mp->COND = 1110 << 28;
 }
