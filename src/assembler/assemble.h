e940137b59d3d850993c0f0d1a11d1e61b87d07c
klc116 <klc116@ic.ac.uk>
2017-06-06 17:12:17 +0100

    included emulate.h in the assemble header

--- src/assembler/assemble.h
+++ src/assembler/assemble.h
@@ -49,59 +49,6 @@ void assembleB(LINE_TOKEN* line_token, INSTRUCTION* instr);
 void assembleLsl(LINE_TOKEN* line_token, INSTRUCTION* instr);
 void assembleAndeq(LINE_TOKEN* line_token, INSTRUCTION* instr);
 
-typedef struct {
-    u32 INSTRUCTION;
-    char OPCODE[4];
-    u32 COND: 4;
-    u32 S:1;
-    u32 I:1;
-    u32 DEST;
-    u32 SRC;
-    u32 OPERAND2;
-}DATAPROCESSING_INSTR;
-
-typedef struct {
-    u32 INSTRUCTION;
-    char OPCODE[4];
-    u32 COND: 4;
-    u32 S:1;
-    u32 A:1;
-    u32 DEST;
-    u32 REGM;
-    u32 REGS;
-    u32 ACC;
-}MULTIPLY_INSTR;
-
-typedef struct {
-    u32 INSTRUCTION;
-    char OPCODE[4];
-    u32 COND: 4;
-    u32 REGD;
-    u32 REGN;
-    u32 OFFSET;
-    u32 I: 1;
-    u32 P: 1;
-    u32 U: 1;
-    u32 L: 1;
-}SIN_DATA_TRAN_INSTR;
-
-typedef struct {
-    u32 INSTRUCTION;
-    char OPCODE[4];
-    u32 COND: 4;
-    u32 OFFSET;
-}BRANCH_INSTR;
-
-typedef struct {
-    char type[20];
-    union {
-        DATAPROCESSING_INSTR* dp;
-        MULTIPLY_INSTR* mp;
-        SIN_DATA_TRAN_INSTR* sdt;
-        BRANCH_INSTR* br;
-    }instr;
-}INSTRUCTION;
-
 struct Linkedlist* getNewlist(void);
 struct Linkedlist* insertElementInNode(struct Linkedlist* list,char *key, void* value);
 void* lookUpValue(struct Linkedlist *list, char *key);
