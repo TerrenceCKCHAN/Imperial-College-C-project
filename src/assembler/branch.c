614a8cf469bdf486e0f44c2ef23a9ec8159cba58
Cheuk Ki Fung <ki.cheuk16@imperial.ac.uk>
2017-06-06 15:51:00 +0100

    very basic of sdt n branch

--- src/assembler/branch.c
+++ src/assembler/branch.c
@@ -1,4 +1,10 @@
 //
 // Created by Timothy Cheuk on 06/06/2017.
 //
+u32 branch (LINE_TOKEN* token){
+    u32 temp;
 
+
+    temp = cond <<
+    return temp;
+}
--- src/assembler/sin_data_transfer.c
+++ src/assembler/sin_data_transfer.c
@@ -5,8 +5,32 @@
 
 u32 sin_data_transfer(LINE_TOKEN* token ){
     u32 temp;
-    temp += lookuptable(token->opcode)<<31;
-    temp += 
+    u32 cond;
+    u32 i;
+    u32 P;
+    u32 U;
+    u32 L;
+    u32 Rn;
+    u32 Rd;
+    u32 Offset;
+    if( token->opcode == ){
+        L = 1;
+        if (token->operands[1]<= 0xff){
+            printf("shdnt end up here in sdt, shd go to mov instead");
+        }
+        else{
+            Rd = loouptabler(token->operands[0]);
+            i = 1;
+            p = 1;
+            U = 1;
+            Rn = 0xf;
+            Rd = 0;
+            Offset =
+        }
+    }
+    else{
+        L = 0;
+    }
 
 
 
@@ -16,6 +40,10 @@ u32 sin_data_transfer(LINE_TOKEN* token ){
 
 
 
+
+
+
+    temp = cond<<27 + 1<<25 + I << 24 + P << 23 + U << 22 + Rn << 15 + Rd << 11 + Offset;
     return u32;
 
 }
