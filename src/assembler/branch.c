a83b4b1fdbe556a877dae4ef83cb7f2fef2e1c9d
Cheuk Ki Fung <ki.cheuk16@imperial.ac.uk>
2017-06-07 22:31:54 +0100

    branch: update for current address

--- src/assembler/branch.c
+++ src/assembler/branch.c
@@ -17,55 +17,56 @@ u32 transformnum(u32 num){
 }
 
 //as by the time i finish this part we still not able to get current address, create this helper so we can amend easier
-void generalbr(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void generalbr(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     u32 i = (u32) lookUpValue(symboltable, line_token->operands[0]);
+    i=i-curA-8;
     instr->instr.br->OFFSET = transformnum(i);
     strcpy(instr->type, "branch");
 }
 
 
-void assembleBeq(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleBeq(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"beq");
     instr->instr.br->COND = 0x0;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
-void assembleBne(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleBne(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"bne");
     instr->instr.br->COND = 0x1;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
-void assembleBge(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable) {
+void assembleBge(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA) {
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"bge");
     instr->instr.br->COND = 0xA;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
-void assembleBlt(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleBlt(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"blt");
     instr->instr.br->COND = 0xB;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
-void assembleBgt(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleBgt(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"bgt");
     instr->instr.br->COND = 0xC;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
-void assembleBle(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleBle(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"ble");
     instr->instr.br->COND = 0xD;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
 
-void assembleB(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable){
+void assembleB(LINE_TOKEN* line_token, INSTRUCTION* instr,struct Linkedlist *symboltable,u32 curA){
     instr->instr.br = malloc(sizeof(BRANCH_INSTR));
     strcpy(instr->instr.br->OPCODE,"b");
     instr->instr.br->COND = 0xE;
-    generalbr(line_token, instr,symboltable);
+    generalbr(line_token, instr,symboltable,curA);
 }
 
 
