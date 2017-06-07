33ec28d0ed058fa1240806089d99c47adb90c43a
ckc116 <ckc116@ic.ac.uk>
2017-06-07 19:24:32 +0100

    Basic set up for RaspBerryPi #2

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,5 +1,26 @@
+
+// Data Section
+.balign 4
+var1:
+    .word 3
+
+.balign 4
+var2:
+    .word 4
+// Code Section
+.text
+.balign 4
+
 .global main
 
 main:
-    mov r0, #2 //r0 = value 2;
-    bx lr;     //exit main function
+    ldr r1, addr_of_var1 // r1 <- &var1
+    ldr r1, [r1]         // r1 <- *r1
+    ldr r2, addr_of_var2 // r1 <- &var2
+    ldr r2, [r2]         // r2 <- *r2
+    add r0, r1, r2
+    bx lr
+
+//Labels needed to acess data
+addr_of_var1: .word var1
+addr_of_var2: .word var2
