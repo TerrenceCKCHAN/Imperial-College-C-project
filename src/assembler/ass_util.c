0e505aa90e9728889beb568c352dc4bb43794721
klc116 <klc116@ic.ac.uk>
2017-06-07 17:00:18 +0100

    modified ass_util.c so that it can output the correct value instead of all shifted right by one bit

--- src/assembler/ass_util.c
+++ src/assembler/ass_util.c
@@ -24,18 +24,18 @@ u32 assembleInstructions(INSTRUCTION* instr) {
         instruction |= instr->instr.mp->COND << 28;
     } else if(strcmp(instr->type, "singledatatransfer") == 0) {
         instruction |= instr->instr.sdt->OFFSET;
-        instruction |= instr->instr.sdt->REGD << 11;
-        instruction |= instr->instr.sdt->REGN << 15;
-        instruction |= instr->instr.sdt->L << 19;
-        instruction |= instr->instr.sdt->U << 22;
-        instruction |= instr->instr.sdt->P << 23;
-        instruction |= instr->instr.sdt->I << 24;
-        instruction |= 1 << 25;
-        instruction |= instr->instr.sdt->COND << 27;
+        instruction |= instr->instr.sdt->REGD << 12;
+        instruction |= instr->instr.sdt->REGN << 16;
+        instruction |= instr->instr.sdt->L << 20;
+        instruction |= instr->instr.sdt->U << 23;
+        instruction |= instr->instr.sdt->P << 24;
+        instruction |= instr->instr.sdt->I << 25;
+        instruction |= 1 << 26;
+        instruction |= instr->instr.sdt->COND << 28;
     } else if(strcmp(instr->type, "branch") == 0) {
         instruction |= instr->instr.br->OFFSET;
-        instruction |= 10 << 23;
-        instruction |= instr->instr.br->COND << 27;
+        instruction |= 10 << 24;
+        instruction |= instr->instr.br->COND << 28;
     } else if(strcmp(instr->type, "halt") == 0) {
         instruction = 0;
     }
