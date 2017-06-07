4d8765e9113ace89a2c932fe56692c0538b02f76
ckc116 <ckc116@ic.ac.uk>
2017-06-07 22:29:11 +0100

    First draft of Raspberry assembly program completed

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,64 +1,87 @@
 
-// Data Section
-.balign 4
-var1:
-    .word 3
+    main:
+                 ldr r4, =0x0
+    for_LED:
+                 cmp r4, #0x100
+                 bge end_LED
 
-.balign 4
-var2:
-    .word 4
-// Code Section
-.text
-.balign 4
-
-.global main
-
-main:
-    ldr r1, addr_of_var1 // r1 <- &var1
-    ldr r1, [r1]         // r1 <- *r1
-    ldr r2, addr_of_var2 // r1 <- &var2
-    ldr r2, [r2]         // r2 <- *r2
-    add r0, r1, r2
+    //Clearing pin 16 - write a 1 into pin 16 - set 18th bit at address 0x2020 0028 to 1
+                 ldr r2, =0x40000
+                 str r2, 0x20200028
 
     // Delay LED switching
     for_loop_x:
-        mov r1, #0
+                 ldr r1, =0x0
 
     next_x:
-        cmp r1, #10000
-        bge endfor
+                 cmp r1, #0x2710
+                 bge endfor
 
     increment_x:
-        add r1, #1
-        b next_x
+                 add r1, r1, #0x1
+                 b next_x
 
     for_loop_y:
-        mov r2, #10000
+                 ldr r2, =0x2710
 
     next_y:
-        cmp r2, #10000
-        bge increment_x
+                 cmp r2, #0x2710
+                 bge increment_x
 
     increment_y:
-        add r2, #1
-        b next_y
+                 add r2, r2, #0x1
+                 b next_y
 
     for_loop_z:
-        mov r3, #0
-        cmp r3, #10000
-        bge increment_y
-        add r3, #1
-        b for_loop_z
+                 ldr r3, =0x0
+                 cmp r3, #0x2710
+                 bge increment_y
+                 add r3, r3, #0x1
+                 b for_loop_z
 
     endfor:
     //end of delay
 
+    //Setting pin 16
+                 ldr r2, =0x40000
+                 str r2, =0x20200004
+    // turning on pin 16
+                 str r2, =0x2020001C
+
+       // Delay LED switching
+    for_loop_x:
+                 ldr r1, =0x0
+    next_x:
+                 cmp r1, #0x2710
+                 bge endfor
+
+    increment_x:
+                 add r1, r1, #0x1
+                 b next_x
+
+    for_loop_y:
+                 ldr r2, =0x2710
+
+    next_y:
+                 cmp r2, #0x2710
+                 bge increment_x
+
+    increment_y:
+                 add r2, r2, #0x1
+                 b next_y
+
+    for_loop_z:
+                 ldr r3, =0x0
+                 cmp r3, #0x2710
+                 bge increment_y
+                 add r3, r3, #0x1
+                 b for_loop_z
+
+    endfor:
+    //end of delay
+
+                 add r4, r4, #1;
+    end_LED:
+                 bx lr
 
 
-
-
-    bx lr
-
-//Labels needed to acess data
-addr_of_var1: .word var1
-addr_of_var2: .word var2
