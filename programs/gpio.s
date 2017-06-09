aaafebadd6190724748792b88e0f94d629d7dc8c
ckc116 <ckc116@ic.ac.uk>
2017-06-09 18:10:03 +0100

    RaspberryPi without comments

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,20 +1,15 @@
 
     main:
                  ldr r4, =0x0
-
-    //Setting pin 16 as an output pin
                  ldr r2, =0x40000
                  str r2, =0x20200004
 
     for_LED:
                  cmp r4, #0x100
                  bge end_LED
-
-    //Clearing pin 16 - write a 1 into pin 16 - set 18th bit at address 0x2020 0028 to 1
                  ldr r2, =0x40000
                  str r2, 0x20200028
 
-    // Delay LED switching
     for_loop_x:
                  ldr r1, =0x0
 
@@ -45,13 +40,10 @@
                  b for_loop_z
 
     endfor:
-    //end of delay
 
-    // turning on pin 16
                  ldr r2, =0x40000
                  str r2, =0x2020001C
 
-       // Delay LED switching
     for_loop_x:
                  ldr r1, =0x0
     next_x:
@@ -81,8 +73,6 @@
                  b for_loop_z
 
     endfor:
-    //end of delay
-
                  add r4, r4, #1;
     end_LED:
                  bx lr
