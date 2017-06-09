80f0d4e4af44193f3a82c14f4cfc2b3905bae91e
ckc116 <ckc116@ic.ac.uk>
2017-06-09 18:40:11 +0100

    RaspberryPi debug4 : test.s ammended from ldr to mov

--- programs/test.s
+++ programs/test.s
@@ -1,5 +1,5 @@
 main:
                  ldr r4, =0x0
-                 ldr r2, =0x40000
-                 ldr r3, =0x20200004
+                 mov r2, #0x40000
+                 mov r3, #0x20200004
                  str r2, [r3]
