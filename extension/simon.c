ef7e97d079c465ce4db41a4b0a1d8bea390a6142
Ivan Mang <hxm16@ic.ac.uk>
2017-06-15 22:03:04 +0100

    Major Change in Simon game and added comments

--- extension/simon.c
+++ extension/simon.c
@@ -4,112 +4,182 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define CORRECT 	2//RPI_GPIO_P1_03	//GPIO 02
-#define WRONG 		3//RPI_GPIO_P1_05    //GPIO 03
-#define YOUR_TURN 	4//RPI_GPIO_P1_7	//GPIO 04
-#define RED 		17//RPI_GPIO_P1_11	//GPIO 17
-#define YELLOW 		27//RPI_GPIO_P1_13	//GPIO 27
-#define GREEN 		9//RPI_GPIO_P1_21	//GPIO 09
-#define WHITE 		22//RPI_GPIO_P1_15	//GPIO 22
-#define RED_BOTTON 			10//RPI_GPIO_P1_19	//GPIO 10
-#define YELLOW_BOTTON 		11//RPI_GPIO_P1_23	//GPIO 11
-#define WHITE_BOTTON 		14//RPI_GPIO_P1_8	//GPIO 14
-#define GREEN_BOTTON 		15//RPI_GPIO_P1_10	//GPIO 15
-#define COUNTER_3 		25//RPI_GPIO_P1_12	//GPIO 25
-#define COUNTER_2 		24//RPI_GPIO_P1_16	//GPIO 24
-#define COUNTER_1 		23//RPI_GPIO_P1_18	//GPIO 23
-#define COUNTER_0 		18//RPI_GPIO_P1_22	//GPIO 18
+/////////////////////////////////////////////////////////////////////////////////////////
+// Macro definition that help to set the GPIO Pins to their corresponding lightd
+/////////////////////////////////////////////////////////////////////////////////////////
+#define CORRECTLIGHT 			2
+#define WRONGLIGHT				3
+#define YOUR_TURN 				4
+#define REDLIGHT 				17
+#define YELLOWLIGHT 			27
+#define GREENLIGHT 				9
+#define WHITELIGHT				22
+#define RED_BOTTON 				10
+#define YELLOW_BOTTON 			11
+#define WHITE_BOTTON 			14
+#define GREEN_BOTTON 			15
+#define COUNTER_3 				25
+#define COUNTER_2 				24
+#define COUNTER_1 				23
+#define COUNTER_0 				18
 
-int difficulty = 1;
+//indicate the difficulty of current game state
+int difficulty = 0;
 
+//enum to represent the light
 enum light{
-	REDL,
-	YELLOWL,
-	GREENL,
-	WHITEL
+	RED,
+	YELLOW,
+	GREEN,
+	WHITE
 };
 
+//current state of the game
 enum state{
-	CORRECTS,
-	WRONGS,
+	CORRECT,
+	WRONG,
 	WAIT
 };
 
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the wrong indicating light
+//POST: The wrong indicating light blinks and the current game terminate
+/////////////////////////////////////////////////////////////////////////////////////////
 void WRONGLightON(){
 	int i =0;
 	for(;i<10;i++){
-	bcm2835_gpio_write(WRONG,HIGH);
+	bcm2835_gpio_write(WRONGLIGHT,HIGH);
 	delay(200);
-	bcm2835_gpio_write(WRONG,LOW);
+	bcm2835_gpio_write(WRONGLIGHT,LOW);
 	delay(200);
 	}
 	delay(3000);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the correct indicating light
+//POST: The correct indicating light on for 1 second
+/////////////////////////////////////////////////////////////////////////////////////////
 void CORRECTLightON(){
-	bcm2835_gpio_write(CORRECT,HIGH);
+	bcm2835_gpio_write(CORRECTLIGHT,HIGH);
 	delay(1000);
-	bcm2835_gpio_write(CORRECT,LOW);
+	bcm2835_gpio_write(CORRECTLIGHT,LOW);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the wrong indicating light
+//POST: The wrong indicating light on for 1 second
+/////////////////////////////////////////////////////////////////////////////////////////
 void YOUR_TURNLightOn(){
 	bcm2835_gpio_write(YOUR_TURN,HIGH);
 	delay(1000);
 	bcm2835_gpio_write(YOUR_TURN,LOW);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the red light
+//PRE: The time passed is valid
+//POST: The red light is turned on for (time) milliseconds
+/////////////////////////////////////////////////////////////////////////////////////////
 void REDLightOn(int time){
-	bcm2835_gpio_write(RED,HIGH);
+	bcm2835_gpio_write(REDLIGHT,HIGH);
 	delay(time);
-	bcm2835_gpio_write(RED,LOW);
+	bcm2835_gpio_write(REDLIGHT,LOW);
 	delay(time);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the red light
+//PRE: The time passed is valid
+//POST: The red light is turned on for (time) milliseconds
+/////////////////////////////////////////////////////////////////////////////////////////
 void GREENLightOn(int time){
-	bcm2835_gpio_write(GREEN,HIGH);
+	bcm2835_gpio_write(GREENLIGHT,HIGH);
 	delay(time);
-	bcm2835_gpio_write(GREEN,LOW);
+	bcm2835_gpio_write(GREENLIGHT,LOW);
 	delay(time);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the yellow light
+//PRE: The time passed is valid
+//POST: The yellow light is turned on for (time) milliseconds
+/////////////////////////////////////////////////////////////////////////////////////////
 void YELLOWLightOn(int time){
-	bcm2835_gpio_write(YELLOW,HIGH);
+	bcm2835_gpio_write(YELLOWLIGHT,HIGH);
 	delay(time);
-	bcm2835_gpio_write(YELLOW,LOW);
+	bcm2835_gpio_write(YELLOWLIGHT,LOW);
 	delay(time);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the white light
+//PRE: The time passed is valid
+//POST: The white light is turned on for (time) milliseconds
+/////////////////////////////////////////////////////////////////////////////////////////
 void WHITELightOn(int time){
-	bcm2835_gpio_write(WHITE,HIGH);
+	bcm2835_gpio_write(WHITELIGHT,HIGH);
 	delay(time);
-	bcm2835_gpio_write(WHITE,LOW);
+	bcm2835_gpio_write(WHITELIGHT,LOW);
 	delay(time);
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the counter_0 light
+//POST: The counter_0 light is turned on
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_0_ON(){
 	bcm2835_gpio_write(COUNTER_0,HIGH);
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the counter_1 light
+//POST: The counter_1 light is turned on
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_1_ON(){
 	bcm2835_gpio_write(COUNTER_1,HIGH);	
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the counter_2 light
+//POST: The counter_2 light is turned on
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_2_ON(){
 	bcm2835_gpio_write(COUNTER_2,HIGH);	
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn on the counter_3 light
+//POST: The counter_3 light is turned on
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_3_ON(){
 	bcm2835_gpio_write(COUNTER_3,HIGH);	
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn off the counter_0 light
+//POST: The counter_0 light is turned off
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_0_OFF(){
 	bcm2835_gpio_clr(COUNTER_0);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn off the counter_1 light
+//POST: The counter_1 light is turned off
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_1_OFF(){
 	bcm2835_gpio_clr(COUNTER_1);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn off the counter_0 light
+//POST: The counter_0 light is turned off
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_2_OFF(){
 	bcm2835_gpio_clr(COUNTER_2);
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//To turn off the counter_0 light
+//POST: The counter_0 light is turned off
+/////////////////////////////////////////////////////////////////////////////////////////
 void C_3_OFF(){
 	bcm2835_gpio_clr(COUNTER_3);
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//To blink the light on the binary counter
+//POST: The counter is blinked for 5 times
+/////////////////////////////////////////////////////////////////////////////////////////
 void CounterBlink(){
 	int i =0 ;
 	for(;i<5;i++){
@@ -127,37 +197,48 @@ void CounterBlink(){
 	
 }
 
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//To blink the 4 light (RED,YELLOW,GREEN,WHITE) circularly and form a pattern
+//PRE: The time of delay and number of circle
+//POST: The 4 light is blinked for (time) milliseconds each circularly in (circle) of times
+/////////////////////////////////////////////////////////////////////////////////////////
 void RotateLight(int time, int circle){
 	int  i =0;
 	CounterBlink();
 	for(;i<circle;i++){
-		bcm2835_gpio_write(RED,HIGH);
+		bcm2835_gpio_write(REDLIGHT,HIGH);
 		delay(time);
-		bcm2835_gpio_write(RED,LOW);
+		bcm2835_gpio_write(REDLIGHT,LOW);
 		delay(time);
-		bcm2835_gpio_write(YELLOW,HIGH);
+		bcm2835_gpio_write(YELLOWLIGHT,HIGH);
 		delay(time);
-		bcm2835_gpio_write(YELLOW,LOW);
+		bcm2835_gpio_write(YELLOWLIGHT,LOW);
 		delay(time);
-		bcm2835_gpio_write(GREEN,HIGH);
+		bcm2835_gpio_write(GREENLIGHT,HIGH);
 		delay(time);
-		bcm2835_gpio_write(GREEN,LOW);
+		bcm2835_gpio_write(GREENLIGHT,LOW);
 		delay(time);
-		bcm2835_gpio_write(WHITE,HIGH);
+		bcm2835_gpio_write(WHITELIGHT,HIGH);
 		delay(time);
-		bcm2835_gpio_write(WHITE,LOW);
+		bcm2835_gpio_write(WHITELIGHT,LOW);
 		delay(time);
 		
 		
 		
 	}
 }
-
-void AllBlink(){
+/////////////////////////////////////////////////////////////////////////////////////////
+//To blink the 4 light (RED,YELLOW,GREEN,WHITE) circularly and indicate winning of the game
+//POST: The 4 light is blinked for 20 milliseconds each circularly in 100 of times
+/////////////////////////////////////////////////////////////////////////////////////////
+void WINBlink(){
 	RotateLight(20,100);	
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//Indicate start of the game and blink all the light to check if the light is valid
+//POST: All light is blinked
+/////////////////////////////////////////////////////////////////////////////////////////
 void StartGame(){
 	RotateLight(50,5);
 	YOUR_TURNLightOn();
@@ -167,6 +248,10 @@ void StartGame(){
 	
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//Generate a random sequence of light in specific length
+//POST: A random sequence of (length) is generated
+/////////////////////////////////////////////////////////////////////////////////////////
 void initSequence(enum light sequence[], int length){
 	srand(time(NULL));
 	int index =0;
@@ -176,6 +261,10 @@ void initSequence(enum light sequence[], int length){
 	}
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//Print the current score of the player in binary form using corresponding lights
+//POST: The current score of the player is printed by the binary counter
+/////////////////////////////////////////////////////////////////////////////////////////
 void printCounter(int rounds){
 	if(rounds > 16 ){
 		rounds %= 15;
@@ -213,34 +302,40 @@ void printCounter(int rounds){
 		C_3_OFF();
 	}
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+//Print a sequence of light in specific length using its corresponding lights
+//POST: A sequence of light is generated
+/////////////////////////////////////////////////////////////////////////////////////////
 void printSequence(enum light sequence[], int length, int time){
 	int index = 0;
 	for(;index<length;index++){
 		switch(sequence[index]){
-			case REDL:
+			case RED:
 				REDLightOn(time);
 				break;
-			case YELLOWL:
+			case YELLOW:
 				YELLOWLightOn(time);
 				break;
-			case GREENL:
+			case GREEN:
 				GREENLightOn(time);
 				break;
-			case WHITEL:
+			case WHITE:
 				WHITELightOn(time);
 				break;
 				
 		}
 	}
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////
+//Turn off all the light
+/////////////////////////////////////////////////////////////////////////////////////////
 void OFFLight(){
-		bcm2835_gpio_clr(RED);
-		bcm2835_gpio_clr(GREEN);
-		bcm2835_gpio_clr(WHITE);
-		bcm2835_gpio_clr(YELLOW);
-		bcm2835_gpio_clr(CORRECT);
-		bcm2835_gpio_clr(WRONG);
+		bcm2835_gpio_clr(REDLIGHT);
+		bcm2835_gpio_clr(GREENLIGHT);
+		bcm2835_gpio_clr(WHITELIGHT);
+		bcm2835_gpio_clr(YELLOWLIGHT);
+		bcm2835_gpio_clr(CORRECTLIGHT);
+		bcm2835_gpio_clr(WRONGLIGHT);
 		bcm2835_gpio_clr(YOUR_TURN);
 		bcm2835_gpio_clr(COUNTER_0);
 		bcm2835_gpio_clr(COUNTER_1);
@@ -248,51 +343,59 @@ void OFFLight(){
 		bcm2835_gpio_clr(COUNTER_3);
 }
 
-
-char ReadInput(){
+/////////////////////////////////////////////////////////////////////////////////////////
+//To read the input from the player using buttons and turn on the light next to the button
+//PRE: A valid input is provided
+//POST: The input is stored and the corresponging light is turned on for 500 miiliseconds
+/////////////////////////////////////////////////////////////////////////////////////////
+enum light ReadInput(){
 		int time = 500;
+        //the infinite loop take inputs continually
 		while(1){
 		if(bcm2835_gpio_eds(RED_BOTTON)){
 			REDLightOn(time);
 			bcm2835_gpio_set_eds(RED_BOTTON);
-			return 'R';
+			return RED;
 		}
 		if(bcm2835_gpio_eds(GREEN_BOTTON)){
 			GREENLightOn(time);
 			bcm2835_gpio_set_eds(GREEN_BOTTON);
-			return 'G';
+			return GREEN;
 		}
 		if(bcm2835_gpio_eds(YELLOW_BOTTON)){
 			YELLOWLightOn(time);
 			bcm2835_gpio_set_eds(YELLOW_BOTTON);
-			return 'Y';
+			return YELLOW;
 		}
 		if(bcm2835_gpio_eds(WHITE_BOTTON)){
 			WHITELightOn(time);
 			bcm2835_gpio_set_eds(WHITE_BOTTON);
-			return 'W';
+			return WHITE;
 		}
 	}
 }
 
-
-void initOutput()
-{
-	bcm2835_gpio_fsel(WRONG,BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(CORRECT,BCM2835_GPIO_FSEL_OUTP);
+/////////////////////////////////////////////////////////////////////////////////////////
+//Initialize the GPIO pins to output
+/////////////////////////////////////////////////////////////////////////////////////////
+void initOutput() {
+	bcm2835_gpio_fsel(WRONGLIGHT,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(CORRECTLIGHT,BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(YOUR_TURN,BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(RED,BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(YELLOW,BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(GREEN,BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(WHITE,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(REDLIGHT,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(YELLOWLIGHT,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GREENLIGHT,BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(WHITELIGHT,BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(COUNTER_0,BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(COUNTER_1,BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(COUNTER_2,BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(COUNTER_3,BCM2835_GPIO_FSEL_OUTP);
 }
 
-void initInput()
-{
+/////////////////////////////////////////////////////////////////////////////////////////
+//Initialize the GPIO pins to input
+/////////////////////////////////////////////////////////////////////////////////////////
+void initInput() {
 	bcm2835_gpio_fsel(RED_BOTTON,BCM2835_GPIO_FSEL_INPT);
 	bcm2835_gpio_set_pud(RED_BOTTON,BCM2835_GPIO_PUD_UP);
 	bcm2835_gpio_len(RED_BOTTON);
@@ -309,90 +412,82 @@ void initInput()
 }
 
 
-int main(int argc, char **argv)
-{
-	while(1){
-	bcm2835_init();
-	if(!bcm2835_init()){
-		return 1;
-	}
-	
-	enum light sequence[100];
-	enum state gamestate = WAIT;
-	enum light input;
+int main(int argc, char **argv) {
+	//The infinite loop is to restart the game once the player lose
+    while(1){
+        //Initialise the library by opening /dev/mem and getting pointers to the internal memory for BCM 2835 device registers
+	    bcm2835_init();
+        //if the library can't be initialise, the program terminate
+	    if(!bcm2835_init()){
+		    return 1;
+	    }
+	    //Initialise the game state, current round, reserve memory for sequence and input
+	    enum light sequence[100];
+	    enum state gamestate = WAIT;
+	    enum light input;
+	    int round = 1;
+        //Initialise the blinking time to be 450 milliseconds
+	    int time = 450 - difficulty * 150;
 
-	int round = 1;
+	    //Turn off all light
+	    OFFLight();
+        //set the GPIO pin to input or output
+	    initOutput();
+	    initInput();
+	    //generate a random sequence of light
+	    initSequence(sequence,100);
+        //start the game by printing a starting light sequence
+	    StartGame();
+	
+	        while(gamestate != WRONG){
+                //print the sequence of the light generated
+		        printSequence(sequence,round, time);
+                //initialise the current position to 0
+		        int pos = 0;
+                //The your turn light turned on indicate, it is the player's turn
+		        YOUR_TURNLightOn();
+		            while(gamestate == WAIT){
+		                if(pos == round){
+                            //The player input correctly and the correct light is on
+			                CORRECTLightON();
+                            //print the current score
+			                printCounter(round);
+                            //change the game state to correct
+			                gamestate = CORRECT;
+			                break;
+		                }
+                        //read the input from the player and return enum of light
+		            input = ReadInput();
 
-	int time = 450 - difficulty*150;
-
+		                //if the current input match with the sequence
+		                if(sequence[pos] == input){
+                        //move to the next position
+			            pos++;
+		                }else{
+                        //The input didn't match the sequence
+			            gamestate = WRONG;
+                        }
+	                }
+		        if(gamestate == CORRECT){
+                //increment the round and change the game satate
+			    round++;
+			    gamestate = WAIT;
+                }else if(gamestate == WRONG){
+                //The program will terminate and restart after the wrong indicating light blinks
+			     WRONGLightON();
+		        }
+		        if(difficulty == 2){
+                //The player reached level 3 and anf win the game
+			    WINBlink();
+                    break;
+		        }
+	        }
 	
-		
-		
-	
-
-	
-	OFFLight();
-	initOutput();
-	initInput();
-	
-	initSequence(sequence,100);
-	StartGame();
-	
-	 
-	
-	while(gamestate != WRONGS){
-		printSequence(sequence,round, time);
-		int pos = 0;
-		YOUR_TURNLightOn();
-		while(gamestate == WAIT){
-		if(pos == round){
-			CORRECTLightON();
-			printCounter(round);
-			gamestate = CORRECTS;
-			break;
-		}
-		char c = ReadInput();
-		
-		switch(c){
-			case 'R':
-				input = REDL;
-				break;
-			case 'Y':
-				input = YELLOWL;
-				break;
-			case 'G':
-				input = GREENL;
-				break;
-			case 'W':
-				input = WHITEL;
-				break;				
-		}
-		
-		if(sequence[pos] == input){
-			pos++;
-		}else{
-			gamestate = WRONGS;
-		}
-		
-	}
-	
-		if(gamestate == CORRECTS){
-			round++;
-			gamestate = WAIT;
-		}else if(gamestate == WRONGS){
-			WRONGLightON();
-		}
-		if(difficulty == 2){
-			AllBlink();
-			break;
-		}
-	}
-	
-
-	OFFLight();
-	
-	bcm2835_close();
-}
+        //turn off all the light and wait for restart
+	    OFFLight();
+	    //close the library, deallocating any allocated memory and closing /dev/mem
+	    bcm2835_close();
+    }
 	
 	return 0;
 }
